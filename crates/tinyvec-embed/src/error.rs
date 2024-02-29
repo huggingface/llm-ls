d8dc17faff1943e45690bdf84a8c7b541997a9cf
Wats0ns <Wats0ns@users.noreply.github.com>
2024-02-29 11:43:27 +0100

    Update crates/tinyvec-embed/src/error.rs
    Co-authored-by: Luc Georges <McPatate@users.noreply.github.com>

--- crates/tinyvec-embed/src/error.rs
+++ crates/tinyvec-embed/src/error.rs
@@ -36,8 +36,6 @@ pub enum Error {
     ValueNotNumber(Value),
     #[error("expected value to be string, got: {0}")]
     ValueNotString(Value),
-    #[error("expected value to be a valid size, got: {0}")]
-    ValueNotUsize(Value),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
