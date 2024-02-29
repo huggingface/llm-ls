17ea54a3f4e7d2b41de5c1fd4abde77e6b282855
Wats0ns <Wats0ns@users.noreply.github.com>
2024-02-29 11:43:27 +0100

    Update crates/tinyvec-embed/src/error.rs
    Co-authored-by: Luc Georges <McPatate@users.noreply.github.com>

--- crates/tinyvec-embed/src/error.rs
+++ crates/tinyvec-embed/src/error.rs
@@ -32,10 +32,10 @@ pub enum Error {
     InvalidFileName,
     #[error("io error: {0}")]
     Io(#[from] std::io::Error),
+    #[error("expected value to be a valid number, got: {0}")]
+    ValueNotNumber(Value),
     #[error("expected value to be string, got: {0}")]
     ValueNotString(Value),
-    #[error("expected value to be a valid size, got: {0}")]
-    ValueNotUsize(Value),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
